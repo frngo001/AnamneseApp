3e9a4c091e5dbfcdc87e03c29ee70da56579806a
frngo001 <136967951+frngo001@users.noreply.github.com>
2023-06-18 14:03:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,15 +10,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
-app.use(session({
-    name: 'session',
-    secret: 'my_secret',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        maxAge: 3600 * 1000, // 1hr
-    }
-}));
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
