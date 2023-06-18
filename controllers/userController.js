718e98b7b6f6beb6943d5dd1ba3d364fdf1f7e44
Franc Loic <franc.loic.ngongang.fanang@ges.thm.de>
2023-06-18 14:25:54 +0200

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,14 +5,7 @@ const bcrypt = require('bcryptjs');
 
 // Home Page
 exports.homePage = async (req, res, next) => {
-    const [row] = await dbConnection.execute("SELECT * FROM `users` WHERE `id`=?", [req.session.userID]);
-
-    if (row.length !== 1) {
-        return res.redirect('/logout');
-    }
-
-    res.render('home', {
-        user: row[0]
-    });
+  
+    res.render('home');
 }
 
