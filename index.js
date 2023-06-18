0e8d5fed00105d274311c20bd5e76fad04b9b1bb
frngo001 <136967951+frngo001@users.noreply.github.com>
2023-06-18 14:11:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,4 +20,8 @@ app.use((err, req, res, next) => {
    
 });
 
-app.listen(3000, () => console.log('Server is runngin on port 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
